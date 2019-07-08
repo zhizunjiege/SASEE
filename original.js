445c2ef95d1eda369898e1c4d4c0402d2761ef02
DeDiracle <2945161896@qq.com>
2019-07-08 22:07:58 +0800

    7/8最新的版本

--- original.js
+++ original.js
@@ -2,6 +2,7 @@ var express = require('express');
 var fs = require('fs');
 var ejs = require('ejs');
 var app = express();
+var bodyParser = require('body-parser');
 
 path = require('path');
 //view uses html
@@ -9,6 +10,10 @@ app.set('views', __dirname + '/public/html');
 app.engine('.html', ejs.__express);
 app.set('view engine', 'html');
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: false }))
+
+
+var bodyParser = require('body-parser')
 //app.set('port',process.env.PORT||3000);
 
 app.get('/',function(req,res){
@@ -21,6 +26,15 @@ app.get('/1',function(req,res){
 	//var name = req.body.name;
 	//var password = req.body.password;
 });
+app.post('/login',function (req,res	){
+	var account = req.body.account;
+	var password = req.body.password;
+	console.log('account:',account);
+	console.log('password:',password)
+	if(account == password){
+		res.render('student',{title:'登录成功'})
+	}
+})
 //404
 app.use(function(req,res){
 	res.type('text/plain');
