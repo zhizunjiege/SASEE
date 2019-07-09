db87b51c39f5cdb369e0f708bc6b50024315643b
zhizunjiege <2452816044@qq.com>
2019-07-09 22:01:13 +0800

    增加客户端逻辑，拉取通知的具体内容

--- public/js/main.js
+++ public/js/main.js
@@ -1,5 +1,19 @@
-$(document).ready(function(){
-    $(".aaa").keypress(function(event){
-        console.log(event.which);
-    })
-});
+(function ($) {
+    const NEWS_URL = "../doc/notice.html";
+
+    $(function ($) {
+        //绑定点击news的行为
+        var newsList=null;
+        $("#news_list>li").click(function () {
+            if (!$(this).hasClass("active")) {
+                $("#news_content").load(NEWS_URL, "num=" + $(this).index(), function () {
+                    newsList=$(this).next().remove();
+                });
+            }
+            $(this).toggleClass("active");
+        });
+
+    });
+})(window.jQuery);
+
+
