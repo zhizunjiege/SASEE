b3f860f77f65997ce1b86dd87d04d0d5e448fdd1
zhizunjiege <2452816044@qq.com>
2019-07-10 21:46:40 +0800

    加入ejs模板引擎

--- original.js
+++ original.js
@@ -16,33 +16,39 @@ app.use(bodyParser.urlencoded({ extended: false }))
 var bodyParser = require('body-parser')
 //app.set('port',process.env.PORT||3000);
 
-app.get('/',function(req,res){
-	res.render('login', {title:'paint title'});
+app.get('/', function (req, res) {
+	res.render('login', { title: 'paint title' });
 	//var name = req.body.name;
 	//var password = req.body.password;
 });
-app.get('/1',function(req,res){
-	res.render('student', {title:'paint title'});
+app.get('/1', function (req, res) {
+	res.render('student', { title: 'paint title' });
 	//var name = req.body.name;
 	//var password = req.body.password;
 });
-app.get('/login',function (req,res	){
-	var account = req.body.account;
-	var password = req.body.password;
-	console.log('account:',account);
-	console.log('password:',password)
-	if(account == password){
-		res.render('student',{title:'登录成功'})
+app.get('/login', function (req, res) {
+	var account = req.query.account;
+	var password = req.query.password;
+	console.log('account:' + account);
+	console.log('password:' + password);
+	if (account == password) {
+		res.render('student', {
+			user: {
+				profile: 'profile',
+				name: '333',
+				identity: req.query.identity ? '教师' : '学生',
+			}
+		})
 	}
 })
 //404
-app.use(function(req,res){
+app.use(function (req, res) {
 	res.type('text/plain');
 	res.status(404);
 	res.send('404 ！- Not Found');
 });
 
-app.listen(3000,'0.0.0.0',function(){
+app.listen(3000, '0.0.0.0', function () {
 	console.log('express is running on localhost:3000')
 
 });
