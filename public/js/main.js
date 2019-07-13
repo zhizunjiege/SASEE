f80b014e9251731e6b1c5aed81a2787e8f7ad03d
zhizunjiege <2452816044@qq.com>
2019-07-13 15:23:37 +0800

    前端主要逻辑基本完成

--- public/js/main.js
+++ public/js/main.js
@@ -1,6 +1,6 @@
 (function ($) {
-    const URL_NEWS_CONTENT = '../doc/notice.html';
-    const URL_NEWS_LIST = '../doc/view';
+    const URL_NOTICE = '../doc/notice.html';
+    const URL_VIEW = '../view';
 
     $(function ($) {
         //绑定点击news的行为
@@ -11,7 +11,7 @@
         };
 
         $('#news_list>ul>li').click((e) => {
-            $('#news_content').load(URL_NEWS_CONTENT, 'type=newsContent&' + 'num=' + $(e.currentTarget).index(), newsToggle);
+            $('#news_content').load(URL_NOTICE, 'type=notice&' + 'num=' + $(e.currentTarget).index(), newsToggle);
         });
         $('#news_header>button').click(newsToggle);
 
@@ -21,10 +21,10 @@
         var nextPage = 1;
 
         function pageNavToggle(e) {
-            $('#news_list').load(URL_NEWS_LIST, 'type=newsList&' + 'nextPage=' + nextPage, () => {
+            $('#news_list').load(URL_VIEW, 'type=newsList&' + 'nextPage=' + nextPage, () => {
                 if (pageNum > 1) {
                     var sel = $('#page_nav>ul>li');
-                    var already=true;
+                    var already = true;
                     sel.each((index, element) => {
                         if ($(element).text() == currentPage.toString()) {
                             $(element).toggleClass('active');
@@ -34,13 +34,13 @@
                     sel.each((index, element) => {
                         if ($(element).text() == nextPage.toString()) {
                             $(element).toggleClass('active');
-                            already=false;
+                            already = false;
                             return false;
                         } else return true;
                     });
-                    if(already){
+                    if (already) {
                         $('#page_jump').replaceWith($('#page_jump').prev().clone());
-                        $('#page_nav>ul>li').eq(2).attr('id','page_jump').toggleClass('active').children('a').text(nextPage);
+                        $('#page_nav>ul>li').eq(2).attr('id', 'page_jump').toggleClass('active').children('a').text(nextPage);
                         //$('#page_jump').removeClass('dropup').html($('#page_jump').prev().clone().children('a').text(nextPage)).toggleClass('active');
                     }
                     if (currentPage == 1 || nextPage == 1) {
@@ -87,9 +87,45 @@
                 default:
                     if (!$(e.target).parent().hasClass('active')) {
                         nextPage = Number(e.target.innerText);
-                        pageNavToggle();
+                        if (!isNaN(nextPage)) {
+                            pageNavToggle();
+                        }
                     }
                     break;
+            }
+        });
+
+        //个人信息的加载
+        $('#toggle_user_info').click((e)=>{
+            var href=$(e.currentTarget).attr('href');
+            if (!$(href).length) {
+                $('<div>', {
+                    "id": href.substring(1),
+                    "class": "collapse",
+                    "data-parent": "#content"
+                }).appendTo('#content').collapse({
+                    parent:'#content',
+                    toggle:true
+                }).load(URL_VIEW, 'type=userInfo', (e) => {
+                    console.log(e);
+                })
+            }
+        });
+
+        //各个阶段的加载
+        $('#navigator ul>a').click((e) => {
+            var periodId = $(e.currentTarget).attr('href');
+            if (!$(periodId).length) {
+                $('<div>', {
+                    "id": periodId.substring(1),
+                    "class": "collapse",
+                    "data-parent": "#content"
+                }).appendTo('#content').collapse({
+                    parent:'#content',
+                    toggle:true
+                }).load(URL_VIEW, 'type=period&' + 'id=' + periodId.substring(1), (e) => {
+                    console.log(e);
+                })
             }
         });
     });
