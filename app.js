ff6e93f8320c86946a548eb578c8c3b8331fe0ae
DeDiracle <2945161896@qq.com>
2019-08-31 12:33:48 +0800

    final 1.0

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const ejs = require('ejs');
 const path = require('path');
 const bodyParser = require('body-parser');
-const cookieParser=require("cookie-parser");
+const cookieParser = require("cookie-parser");
 const SqlString = require('sqlstring');
 const app = express();
 
@@ -16,39 +16,31 @@ app.set('views', __dirname + '/views');
 app.engine('.ejs', ejs.__express);
 app.set('view engine', 'ejs');
 app.use(express.static(path.join(__dirname, 'public')));
-app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.urlencoded({extended: false}));
 app.use(cookieParser());
 
 
 app.get('/', function (req, res) {
-    res.render('login', { title: 'paint title' });
+    res.render('login', {title: 'paint title'});
 });
 
 app.post('/login', function (req, res) {
-    login(req,res);
+    login(req, res);
 });
 
 app.get('/views', (req, res) => {
-    view(req,res)
+    view(req, res)
 });
 
 app.get('/cookie', (req, res) => {
     res.send(req.cookies)
 });
 
-app.post('/upload',(req,res)=>{
-    upload(req,res)
+app.post('/upload', (req, res) => {
+    upload(req, res)
 });
 
-app.post('/choose',(req,res)=>{
-    let group_term = [
-        ['王', 'duan'],
-        ['yu', 'tao'],
-        ['qian', 'wu'],
-        ['wang', 'xie'],
-        ['chen', 'zhao'],
-        ['lao', 'du']
-    ];
+app.post('/choose', (req, res) => {
     let info = {};
     let account = req.body.account;
     let selection = req.body.group;
@@ -58,51 +50,50 @@ app.post('/choose',(req,res)=>{
         if (err) return res.send('账号错误');
         let group = data[0].group;
         let category = data[0].category;
-
-        info.teacher = group_term[category][selection];
         info.group = selection;
 
         let choose_sql = SqlString.format('UPDATE final  SET ? WHERE account = ?', [info, account]);
 
         let id = category.toString() + selection.toString();
-        console.log('group, selection', group, selection);
-        console.log(typeof(selection));
-        //if(selection == '') return console.log('none')//res.end('3')
+        //console.log('group, selection', group, selection);
+        //console.log(typeof (selection));
+        if (selection == '') {
+            res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+            return res.end('你未进行任何选择');
+        }//res.end('3')}
         if (group == -1) {
-                let pd = "SELECT * FROM g WHERE id = ?";
-                mysql.query(pd, id, function (err, data) {
-                    if (err) return res.send('请稍后尝试');
-                    let chosen = data[0].chosen;
-                    let capacity = data[0].capacity;
-                    if (chosen < capacity){
-                        let sql = "UPDATE g SET chosen = chosen + 1 WHERE id = ?";
-                        mysql.query(sql, id, function (err, data) {
-                            if(err) return res.send('选课失败，请稍后重试');
-                            else {
-                                mysql.query(choose_sql, [], function (err, data) {
-                                    if (err) return res.send('选课失败，请返回重试');
-                                    res.end('0')
-                                    console.log('succeed')
-                                })
-                            }
-                        })
-                    } else return res.end('1')
-                })
-        }
-        else if (group == selection)
-            return res.end('2');
+            let pd = "SELECT * FROM g WHERE id = ?";
+            mysql.query(pd, id, function (err, data) {
+                if (err) return res.send('请稍后尝试');
+                let chosen = data[0].chosen;
+                let capacity = data[0].capacity;
+                if (chosen < capacity) {
+                    let sql = "UPDATE g SET chosen = chosen + 1 WHERE id = ?";
+                    mysql.query(sql, id, function (err, data) {
+                        if (err) return res.send('选课失败，请稍后重试');
+                        else {
+                            mysql.query(choose_sql, [], function (err, data) {
+                                if (err) return res.send('选课失败，请返回重试');
+                                res.end('选择成功！')
+                                console.log('succeed')
+                            })
+                        }
+                    })
+                } else return res.end('容量已满，请选择其他分组！')
+            })
+        } else if (group == selection)
+            return res.end('您已经选择过该分组！');
         else {
             let old_id = category.toString() + group.toString();
             let clean_sql = "UPDATE g SET chosen = chosen - 1 WHERE id = ?";
             mysql.query(clean_sql, old_id, function (err, data) {
                 if (err) return console.log(err);
-                console.log('cleaned')
                 let sql = "UPDATE g SET chosen = chosen + 1 WHERE id = ?";
                 mysql.query(sql, id, function (err, data) {
                     if (err) return console.log(err);
                     mysql.query(choose_sql, [], function (err, data) {
                         if (err) return res.send('选课失败，请返回重试');
-                        else return res.end('0');
+                        else return res.end('选择成功！');
                     })
                 })
             });
@@ -113,7 +104,7 @@ app.post('/choose',(req,res)=>{
 
 //404
 app.use(function (req, res) {
-    NotFound(req,res)
+    NotFound(req, res)
 });
 
 app.listen(3000, '::', function () {
--- routes/login.js
+++ routes/login.js
@@ -1,8 +1,9 @@
 const mysql = require("./sql");
+
 function get_group(id) {
     switch (id) {
-       case -1 :
-           return '未选择';
+        case -1 :
+            return '未选择';
         case 0 :
             return '第一组';
         case 1:
@@ -12,46 +13,57 @@ function get_group(id) {
     }
 }
 
-function login(req,res) {
+function login(req, res) {
     let not = [];
     const account = req.body.account;
     const password = req.body.password;
     let login_sql = "SELECT * FROM final WHERE account = ?";
     mysql.query(login_sql, account, function (err, data) {
-        if (err) {res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-            return res.end('连接失败，请稍后重试')};
-        const result1 = JSON.parse(JSON.stringify(data));
-        console.log(result1);
-        console.log('type',typeof (result1));
-        if (result1.length == 0) {console.log('this block')
+        if (err) {
             res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-            return res.end('该用户不存在，请检查输入')};
-        let pass = account + result1[0].class;
+            return res.end('连接失败，请稍后重试')
+        }
+        ;
+        const result1 = JSON.parse(JSON.stringify(data));
+        //console.log(result1);
+        //console.log('type', typeof (result1));
+        if (result1.length == 0) {
+            //console.log('this block')
+            res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+            return res.end('该用户不存在，请检查输入')
+        }
+        ;
+        let identity=(result1[0].identity||'').slice(-4)||'';
+        //console.log(identity);
+        let pass = account + result1[0].class+identity;
         if (pass == password) {
-            let sql = "SELECT * FROM g WHERE category = ?";
+            let sql = "SELECT * FROM g WHERE category = ? order by id asc";
             mysql.query(sql, result1[0].category, function (err, data) {
                 if (err) return res.end('网络出了点差错，请稍后重试');
                 //console.log('data2',data)
                 let result2 = JSON.parse(JSON.stringify(data));
                 delete result2.category;
                 delete result2.id;
-                console.log('result2',result2);
+                //console.log('result2', result2);
                 //console.log('result1',result1);
-                 res.render('user', {
+                res.render('user', {
                     user: {
-                        account:result1[0].account,
-                        direction:result1[0].category,
+                        account: result1[0].account,
+                        direction: result1[0].category,
                         name: result1[0].name,
                         profile: result1[0].profile,
-                        group:result1[0].group
+                        group: result1[0].group
                     },
-                    groups:result2
-            });
-        })
-        }else {res.setHeader('Content-Type', 'text/plain;charset=utf-8');
-            return res.end("密码错误，请返回重新输入")}
+                    groups: result2
+                });
+            })
+        } else {
+            res.setHeader('Content-Type', 'text/plain;charset=utf-8');
+            return res.end("密码错误，请返回重新输入")
+        }
     });
 }
-    module.exports = login;
+
+module.exports = login;
 
 
