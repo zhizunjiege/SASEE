2d4e4e1aa783c29f6d8e35971e4a313074820e90
zhizunjiege <2452816044@qq.com>
2020-10-06 15:35:02 +0800

    修改PM2配置，适应不同环境

--- pm2.config.js
+++ pm2.config.js
@@ -1,28 +1,40 @@
+const { json } = require("express");
+
+let common = {
+  name: 'app',
+  script: 'app.js',
+  watch: [
+    'dist',
+    'app.js',
+    'scripts',
+    'modules/index.js',
+    'modules/*/index.js',
+    'modules/*/reset.js'
+  ],
+  ignore_watch: [
+    "dist/img"
+  ],
+  env: {
+    NODE_ENV: 'development'
+  },
+  env_production: {
+    NODE_ENV: 'production'
+  },
+  error_file: 'logs/err.log',
+  out_file: 'logs/out.log',
+  // exec_mode: "cluster",
+  // instances: 8,
+  shutdown_with_message: true
+};
+
+let dev = common;
+dev.name = "dev";
+
+let prod = JSON.parse(JSON.stringify(common));
+prod.name = "prod";
+prod.exec_mode = "cluster";
+prod.instances = 8;
+
 module.exports = {
-  apps: [{
-    name: 'app',
-    script: 'app.js',
-    watch: [
-      'dist',
-      'app.js',
-      'scripts',
-      'modules/index.js',
-      'modules/*/index.js',
-      'modules/*/reset.js'
-    ],
-    ignore_watch: [
-      "dist/img"
-    ],
-    env: {
-      NODE_ENV: 'development'
-    },
-    env_production: {
-      NODE_ENV: 'production'
-    },
-    error_file: 'logs/err.log',
-    out_file: 'logs/out.log',
-    // exec_mode: "cluster",
-    // instances: 8,
-    shutdown_with_message: true
-  }]
+  apps: [dev, prod]
 };
